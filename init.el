a595e8977f84bc6f7c246793ea3531e0de06d91b
Luka Novsak <lnovsak@gmail.com>
2018-03-29 19:48:34 +0200

    then there was something

--- init.el
+++ init.el
@@ -1,1 +1,26 @@
+(require 'package)
+
+(defvar required-packages '(diminish use-package))
+
+(defvar scratch-file
+  (expand-file-name "scratch.el" user-emacs-directory))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+(setq use-package-always-ensure t)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(dolist (required-package required-packages)
+  (unless (package-installed-p required-package)
+    (package-refresh-contents)
+    (package-install required-package)))
+
+(if (file-exists-p custom-file) (load custom-file))
+
 (org-babel-load-file "~/.emacs.d/emacs.org")
+
+(if (file-exists-p scratch-file) (load scratch-file))
